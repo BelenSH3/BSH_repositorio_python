29cbfb6f6fa9ae1afce4d17a6a079761b6f815a9
BelenSH <belenrubisantiagohernandez@gmail.com>
2021-05-10 19:20:51 -0600

    Create ciclo_whileEjemplo.py
    ejemplosWhile

---
+++ ciclo_whileEjemplo.py
@@ -0,0 +1,15 @@
+#aqui se controla el ciclo con un input osea con lo que capture el usuario
+num_1=1
+while num_1<=3:
+        #instrucciones
+        salir=(input("Desea salir?"))
+        if salir=="a" or salir=="A":
+                break
+        
+       
+
+#aqui el ciclo se detiene por medio de una variable
+b=1
+while b<=3:
+        b=b+1
+        print(b)
